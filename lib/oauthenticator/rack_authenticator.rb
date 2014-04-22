89bdd6afc4f6e1c08a9002e37996c5ebe22735d6
Ethan <ethan@unth>
2014-04-22 16:44:04 -0400

    minor: naming; this doesn't actually do authorization, just authentication

--- lib/oauthenticator/rack_authenticator.rb
+++ lib/oauthenticator/rack_authenticator.rb
@@ -14,7 +14,7 @@ module OAuthenticator
     # options:
     #
     # - `:bypass` - a proc which will be called with a Rack::Request, which must have a boolean result. 
-    #   if the result is true, authorization checking is bypassed. if false, the request is authenticated 
+    #   if the result is true, authentication checking is bypassed. if false, the request is authenticated 
     #   and responds 401 if not authenticated.
     #
     # - `:config_methods` - a Module which defines necessary methods for an {OAuthenticator::SignedRequest} to 
@@ -42,7 +42,7 @@ module OAuthenticator
         oauth_signed_request_class = OAuthenticator::SignedRequest.including_config(@options[:config_methods])
         oauth_request = oauth_signed_request_class.from_rack_request(request)
         if oauth_request.errors
-          unauthorized_response({'errors' => oauth_request.errors})
+          unauthenticated_response({'errors' => oauth_request.errors})
         else
           env["oauth.consumer_key"] = oauth_request.consumer_key
           env["oauth.token"] = oauth_request.token
@@ -52,10 +52,10 @@ module OAuthenticator
       end
     end
 
-    # the response for an unauthorized request. the argument will be a hash with the key 'errors', whose value 
-    # is a hash with string keys indicating attributes with errors, and values being arrays of strings 
+    # the response for an unauthenticated request. the argument will be a hash with the key 'errors', whose 
+    # value is a hash with string keys indicating attributes with errors, and values being arrays of strings 
     # indicating error messages on the attribute key.. 
-    def unauthorized_response(error_object)
+    def unauthenticated_response(error_object)
       # default to a blank realm, I suppose
       realm = @options[:realm] || ''
       response_headers = {"WWW-Authenticate" => %Q(OAuth realm="#{realm}"), 'Content-Type' => 'application/json'}
