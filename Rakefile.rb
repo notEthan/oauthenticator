e9ea84188c8285a13f174ae060388b748dece2a4
Ethan <ethan@unth>
2016-07-28 22:06:51 -0700

    use wwtd as default rake task

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,7 +4,8 @@ Rake::TestTask.new do |t|
   t.test_files = FileList['test/**/*_test.rb']
   t.verbose = true
 end
-task 'default' => 'test'
+require 'wwtd/tasks'
+task 'default' => 'wwtd'
 
 require 'yard'
 YARD::Rake::YardocTask.new do |t|
