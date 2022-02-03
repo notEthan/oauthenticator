515469ea28333aff235ab1bff2fbe208e51cf277
Ethan <ethan@unth.net>
2022-02-03 02:36:08 -0800

    add rake task gig
    https://github.com/notEthan/gig/blob/v0.0.4/lib/gig.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -12,3 +12,22 @@ YARD::Rake::YardocTask.new do |t|
 end
 
 require 'api_hammer/tasks'
+
+begin
+  require 'gig'
+  gig_loaded = true
+rescue LoadError
+end
+
+if gig_loaded
+  ignore_files = %w(
+    .github/**/*
+    .gitignore
+    Gemfile*
+    config.ru
+    Rakefile.rb
+    oauthenticator.gemspec
+    test/**/*
+  ).map { |glob| Dir.glob(glob, File::FNM_DOTMATCH) }.inject([], &:|)
+  Gig.make_task(gemspec_filename: 'oauthenticator.gemspec', ignore_files: ignore_files)
+end
