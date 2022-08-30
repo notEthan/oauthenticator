f8fba96601fa9028d3051a87dbfa8a471c16b489
Ethan <ethan@unth.net>
2022-08-30 03:44:10 -0700

    minor doc fix: SignableRequest#normalized_request_params may include nil values for pairs

--- lib/oauthenticator/signable_request.rb
+++ lib/oauthenticator/signable_request.rb
@@ -201,7 +201,7 @@ module OAuthenticator
 
     # section 3.4.1.3
     #
-    # @return [Array<Array<String> (size 2)>]
+    # @return [Array<Array<String, nil> (size 2)>]
     def normalized_request_params
       query_params + protocol_params.reject { |k,v| %w(realm oauth_signature).include?(k) }.to_a + entity_params
     end
